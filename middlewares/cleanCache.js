70620278240721c7c93e2cb24034d28a9516a639
Trung DG <thanhtrung.it25@gmail.com>
2021-06-08 15:00:55 +0700

    Travis start

---
+++ middlewares/cleanCache.js
@@ -0,0 +1,7 @@
+const { clearHash } = require('../services/cache');
+
+module.exports = async function(req, res, next) {
+  await next()
+
+  clearHash(req.user.id)
+}
---
+++ tests/factories/sessionFactory.js
@@ -0,0 +1,21 @@
+const Keygrip = require('keygrip')
+const keys = require('../../config/keys')
+const Buffer = require('safe-buffer').Buffer
+
+module.exports = (user) => {
+  const sessionObject = {
+    passport: {
+      user: user._id.toString()
+    }
+  }
+  const session = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  )
+  const keygrip = new Keygrip([keys.cookieKey])
+  const sig = keygrip.sign('session=' + session)
+
+  return {
+    session,
+    sig
+  }
+}
